f80181d4622721783837c02d724ebc59ef1b2e46
MOSTAFIZUR RAHMAN <mostafizur2277@gmail.com>
2022-11-17 21:17:57 +0100

    quest 5 fin

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -4,15 +4,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/program', name:'program_')]
 class ProgramController extends AbstractController
 {
-  #[Route('/program/', name: 'program_index')]
+  #[Route('/', name: 'program_index')]
     public function index():Response
   {
     // return new Response('<html><body>Wild Series Index</body></html>');
     return $this->render('program/index.html.twig', ['website' => 'wild series']);
+
+  }
+
+  #[Route('/{id<\d+>}', methods:['GET'], name: 'program_show')]
+    public function show($id):Response
+  {
+    
+    // return new Response('<html><body>Wild Series Index</body></html>');
+    return $this->render('program/show.html.twig', ['showProgram' => "program $id"]);
 
   }
 }
